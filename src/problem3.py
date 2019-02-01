ce5ad91559f734eefe84dd379e24548b27f1c6a3
myerscar1555 <myerscl@rose-hulman.edu>
2019-01-31 20:16:20 -0500

    DONE

--- src/problem3.py
+++ src/problem3.py
@@ -3,9 +3,9 @@ Exam 3, problem 3.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Carter Myers.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 import time
@@ -182,9 +182,16 @@ def problem3(sequence):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    count = 0
+    for k in range(len(sequence)):
+        if is_prime(sequence[k]) == True:
+            sequence[k] = sequence[k] + 3
+            count = count + 1
+    return count
 
 
 ###############################################################################
